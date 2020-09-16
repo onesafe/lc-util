90bfa9a8ab7b8c554d106ced7c8f9ef22c1fa421
onesafe <onesafe@163.com>
2020-09-16 23:39:38 +0800

    添加工具类

---
+++ Utils.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 第九代封妖师
+ * Date: 2020/09/16
+ */
+public class Utils {
+    public static int inf = Integer.MAX_VALUE;
+    public static int inn = Integer.MIN_VALUE;
+    public static int mod = (int)1e9 + 7;
+}
