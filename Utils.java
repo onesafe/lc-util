32ff42abbc7f0a47e6fae8891ca2defc70fee7d9
onesafe <onesafe@163.com>
2020-09-20 13:03:38 +0800

    方向数组

--- Utils.java
+++ Utils.java
@@ -5,10 +5,16 @@
 public class Utils {
     public static int inf = Integer.MAX_VALUE;
     public static int inn = Integer.MIN_VALUE;
-    public static int mod = (int)1e9 + 7;
+    public static int mod = (int)(1e9 + 7);
+
+    // 方向数组
+    public static int[][] dirs = new int[][]{
+            {1, 0},
+            {0, 1}
+    };
 
     // 求阶乘n!
-    private int fact(int n) {
+    public int fact(int n) {
         int res = 1;
         for(int i=1; i<=n; i++) {
             res *= i;
@@ -17,7 +23,7 @@ public class Utils {
     }
 
     // 从n里面选k个不重复的组合数
-    private int compose(int n, int k) {
+    public int compose(int n, int k) {
         return fact(n) / (fact(n-k)*fact(k));
     }
 }
