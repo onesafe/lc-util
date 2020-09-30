a2415ea3a92c7d1ba74451dd40268d273914dcee
onesafe <onesafe@163.com>
2020-09-30 20:40:20 +0800

    二叉搜索树插入节点

--- tree/TreeUtil.java
+++ tree/TreeUtil.java
@@ -5,6 +5,30 @@ import java.util.*;
 public class TreeUtil {
 
     /**
+     * 二叉搜索树插入节点
+     */
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode insertNode = new TreeNode(val);
+        if (null == root) return insertNode;
+        TreeNode res = root;
+
+        while (insertNode != root) {
+            if (root.val < insertNode.val) {
+                if (null == root.right) {
+                    root.right = insertNode;
+                }
+                root = root.right;
+            } else {
+                if (null == root.left) {
+                    root.left = insertNode;
+                }
+                root = root.left;
+            }
+        }
+        return res;
+    }
+
+    /**
      * 非递归二叉树后续遍历
      */
     public List<Integer> postorderTraversal(TreeNode root) {
