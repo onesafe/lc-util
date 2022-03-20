5d004c6df6f55a5b0ece2a17d3830583b522b8a3
onesafe <onesafe@163.com>
2022-03-20 12:53:51 +0800

    normal 285

---
+++ week/Normal285/L6027.java
@@ -0,0 +1,28 @@
+package week.Normal285;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L6027 {
+    public int countHillValley(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                continue;
+            }
+            list.add(nums[i]);
+        }
+        Integer[] arrs = list.toArray(new Integer[list.size()]);
+        int count = 0;
+        for(int i=0; i<arrs.length-2; i++) {
+            if (arrs[i+1] > arrs[i] && arrs[i+1] > arrs[i+2]) {
+                count++;
+            }
+            if (arrs[i+1] < arrs[i] && arrs[i+1] < arrs[i+2]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ week/Normal285/L6028.java
@@ -0,0 +1,16 @@
+package week.Normal285;
+
+public class L6028 {
+    public int countCollisions(String directions) {
+        char[] arrs = directions.toCharArray();
+        int left = 0, right = arrs.length-1;
+        while(left <= right && arrs[left] == 'L') left++;
+        while(left <= right && arrs[right] == 'R') right--;
+
+        int count = 0;
+        for(int i = left; i <= right; i++) {
+            if (arrs[i] == 'L' || arrs[i] == 'R') count++;
+        }
+        return count;
+    }
+}
